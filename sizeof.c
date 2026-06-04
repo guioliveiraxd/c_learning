8bdc8bd6fcd720897d35beab270fbb3954bd78ae
Guilherme Oliveira <164787576+guioliveiraxd@users.noreply.github.com>
2026-06-03 22:04:04 -0300

    Add short type size output to sizeof.c

--- sizeof.c
+++ sizeof.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
 int main(void) {
-    
+
     int a;
     char b;
     float c;
+    short d;
 
     printf("\n\tint = %d\n", sizeof(int));
     printf("\n\ttchar = %d\n", sizeof(char));
     printf("\n\tfloat = %d\n", sizeof(float));
     printf("\n\tdouble = %d\n", sizeof(double));
+    printf("\n\tshort = %d\n", sizeof(short));
+
+
 
     printf("\n\tint = %d\n", sizeof a);
     printf("\n\tchar = %d\n", sizeof b);
-    printf("\n\tfloat = %d", sizeof c);
+    printf("\n\tfloat = %d\n", sizeof c);
+    printf("\n\tshort = %d\n", sizeof d);
     
     return 0;
 }
